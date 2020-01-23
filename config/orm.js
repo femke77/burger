a9076578676aa9749a801a80dd5e85bb1b8b762e
Meg Meyers <megan.g.meyers@gmail.com>
2020-01-22 20:57:03 -0800

    added some css, burger image

--- config/orm.js
+++ config/orm.js
@@ -8,7 +8,7 @@ var orm = {
         var queryString = "SELECT * FROM ??";
         connection.query(queryString, [tableInput], function (err, result) {
             if (err) throw err;
-            console.log(result);
+            //console.log(result);
             cb(result);
         });
     },
@@ -18,7 +18,7 @@ var orm = {
         var queryString = "INSERT INTO ?? (??) VALUES (?)";
         connection.query(queryString, [tableInput, col, value], function (err, result) {
             if (err) throw err;
-            console.log(result);
+            //console.log(result);
             cb(result);
 
         });
@@ -29,7 +29,7 @@ var orm = {
         var queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
         connection.query(queryString, [tableInput, col1, value1, col2, value2], function (err, result) {
             if (err) throw err;
-            console.log(result);
+            //console.log(result);
             cb(result);
         });
     }
