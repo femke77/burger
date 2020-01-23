4dc65e38a3c68dd2fa50f0b07db0a1c739605491
Meg Meyers <megan.g.meyers@gmail.com>
2020-01-22 17:54:55 -0800

    fixed directory structure, wrote router get and post

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,24 +1,35 @@
 const express = require("express");
 
-var router = express.Router();
+const router = express.Router();
 
 const burger = require("../models/burger.js");
 
-router.get("/", function(req, res) {
-    cat.all(function(data) {
-      var hbsObject = {
-        cats: data
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
+//get all the burgers in the database and send to template as object
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
     });
-  });
+});
+
+router.post("/api/burgers", (req, res) => {
+    burger.insertOne("burger_name", req.body.burger_name, function (result) {
+        res.redirect("/");
+    })
+
+});
+
+router.put("/api/burgers/:id", (req, res) => {
+    var id = req.params.id;
+    //update to devoured with button click. 
+
+});
+
+module.exports = router;
 
 
 
-  module.exports = router;
-
-
-  //get the burgers in the database
   //post a new burger to the database
-  
--- models/burger.js
+++ models/burger.js
@@ -1,28 +1,22 @@
 const orm = require("../config/orm.js");
 
 
-//inside `burger.js`, create the code that will call the ORM functions 
-//using burger specific input for the ORM.
-
 var burger = {
     selectAll: function(cb) {
         orm.selectAll("burgers", function(res) {
             cb(res);
         });
     },
-    insertOne: function(cb) {
+    insertOne: function(col, val, cb) {
         orm.insertOne("burgers", col, val, function(res){
             cb(res);
         });
     },
-    updateOne: function(cb) {
+    updateOne: function(col1, val1, col2, val2, cb) {
         orm.updateOne("burgers", col1, val1, col2, val2, function(res){
             cb(res);
         })
     }
-
 };
-
-
 
 module.exports = burger;
