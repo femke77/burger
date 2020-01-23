39aeb21cf0075d2baebb8c4319e66fed99faa82a
Meg Meyers <megan.g.meyers@gmail.com>
2020-01-22 16:24:18 -0800

    set up server

---
+++ models/burger.js
@@ -0,0 +1,5 @@
+const orm = require("../config/orm.js");
+
+
+//inside `burger.js`, create the code that will call the ORM functions 
+//using burger specific input for the ORM.
--- server.js
+++ server.js
@@ -1,3 +1,28 @@
-const orm = require("./config/orm.js");
+var express = require("express");
+var routes = require("./controllers/burgers_controller.js");
+var exphbs = require("express-handlebars");
 
-orm.updateOne("burgers", "devoured", "1", "burger_name", "Bleu Ribbon Burger")
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Import routes and give the server access to them.
+app.use(routes);
+
+// Set Handlebars.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
+
+
