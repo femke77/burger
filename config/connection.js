49f7e372f93a10908c8e4bfd3565ad43d72d1f37
Meg Meyers <megan.g.meyers@gmail.com>
2020-01-25 14:49:33 -0800

    added jawsdb_url to connection

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,25 @@
 const mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "2hearts",
-    database: "burgers_db"
-});
+if (process.env.JAWSSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "2hearts",
+        database: "burgers_db"
+    })
+}
 
 connection.connect(function (err) {
-    if (err){
+    if (err) {
         console.error("error connecting: " + err.stack);
         return;
     }
     console.log(`Connected to database.\n`)
-    
+
 });
 
 
