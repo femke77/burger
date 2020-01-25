475590a815dfbf15398a5c155fb4b91610d398ce
Meg Meyers <megan.g.meyers@gmail.com>
2020-01-24 16:26:04 -0800

    added ajax post funtion to add Burger btn and styled new section for devoured burgers

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,1 +1,27 @@
-//ajax for post and put
+$(function(){
+
+    $("#addBtn").on("click", (event) => {
+        event.preventDefault();
+      
+
+        var burger = {
+            burger_name: $("#newBurgerBox").val().trim()
+        }
+
+        $.post("/api/burgers", burger, () => {
+            console.log("new burger created");
+            location.reload();
+        });
+
+    });
+
+
+
+
+
+
+
+
+
+
+});
